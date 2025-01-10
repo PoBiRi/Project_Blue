35836e8af20636aabb853bbb775625cee5fe6098
pobijunior <pobijunior@naver.com>
2025-01-11 02:10:51 +0900

    1/10

--- Blue/Assets/Scripts/CameraFollow.cs
+++ Blue/Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;  // 플레이어의 Transform
-    public float smoothSpeed = 0.125f;  // 카메라 이동 부드럽게 하는 속도
+    public float smoothSpeed = 0.5f;  // 카메라 이동 부드럽게 하는 속도
     public Vector3 offset;  // 카메라와 플레이어 간의 위치 차이
 
     public Vector2 minBounds;  // 맵의 최소 경계 (최소 x, 최소 y)
     public Vector2 maxBounds;  // 맵의 최대 경계 (최대 x, 최대 y)
+
+    private Vector3 velocity = Vector3.zero;
 
     void LateUpdate()
     {
@@ -22,6 +24,7 @@ public class CameraFollow : MonoBehaviour
 
         // 제한된 위치로 카메라를 부드럽게 이동
         Vector3 clampedPosition = new Vector3(clampedX, clampedY, desiredPosition.z);
-        transform.position = Vector3.Lerp(transform.position, clampedPosition, smoothSpeed);
+        //transform.position = Vector3.Lerp(transform.position, clampedPosition, smoothSpeed);
+        transform.position = Vector3.SmoothDamp(transform.position, clampedPosition, ref velocity, smoothSpeed);
     }
 }
